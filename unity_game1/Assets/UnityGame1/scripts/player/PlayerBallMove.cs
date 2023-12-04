623b49e5f45d7caf92fcc665a28bf322fca1a0f3
Chocorinku <katetsu5015@gmail.com>
2023-12-04 15:19:23 +0900

    Playerの弾が消える間際にエフェクト追加

--- unity_game1/Assets/UnityGame1/scripts/player/PlayerBallMove.cs
+++ unity_game1/Assets/UnityGame1/scripts/player/PlayerBallMove.cs
@@ -1,29 +1,23 @@
 using UnityEngine;
 
-public class PlayerBallMove : MonoBehaviour
-{
+public class PlayerBallMove : MonoBehaviour {
     public bool facingRight = true;
     private float speed = 18f;
     private float deleteTime = 8f;
     public float ballRate = 5f;
-    void Start()
-    {
+    public GameObject explosion;
+    void Start() {
         Destroy(gameObject, deleteTime);
     }
 
-    void Update()
-    {
-        if (gameObject != null)
-        {
+    void Update() {
+        if (gameObject != null) {
             Vector3 lazerPos = transform.position;
 
             // プレイヤーの向きによって移動方向を切り替え
-            if (facingRight)
-            {
+            if (facingRight) {
                 lazerPos.x += speed * Time.deltaTime;
-            }
-            else
-            {
+            } else {
                 lazerPos.x -= speed * Time.deltaTime;
             }
             transform.Rotate(0f, 0f, ballRate);
@@ -35,7 +29,14 @@ public class PlayerBallMove : MonoBehaviour
         bool isSign = (other.gameObject.CompareTag(Tags.Sign));
         bool isMoveFloor = (other.gameObject.CompareTag(Tags.MoveFloor));
         bool isJumpGimmick = (other.gameObject.CompareTag(Tags.JumpGimmick));
-        if (!isPlayer && !isSign && !isMoveFloor && !isJumpGimmick)
+        if (!isPlayer && !isSign && !isMoveFloor && !isJumpGimmick) {
+            Vector3 pos = transform.position;
+            Quaternion rot = transform.rotation;
+            Instantiate(explosion, pos, rot);
+
+            GameObject exp = GameObject.Find("FX003_01(Clone)");
+            Destroy(exp, 0.2f);
             Destroy(this.gameObject);
+        }
     }
 }
